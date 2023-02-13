228592cb0f4f1ebc0c610ed08a030c1edff97533
Hideki Arima <hide210g@gmail.com>
2023-02-13 17:32:21 +0900

    remove dist

--- packages/backend/dist/modules/root/greet/greet.resolvers.js
+++
@@ -1,9 +0,0 @@
-const resolvers = {
-    Query: {
-        // eslint-disable-next-line @typescript-eslint/require-await
-        greet: async (_, args, context, info) => {
-            return "hello";
-        },
-    },
-};
-export default resolvers;
